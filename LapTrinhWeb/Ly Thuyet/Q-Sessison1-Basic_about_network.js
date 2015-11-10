25ec14835f3ea7a944c36a7d0096a229594b9a62
MinhTam <minhtamck4@gmail.com>
2015-11-10 22:54:26 +0700

    modifine

---
+++ LapTrinhWeb/Ly Thuyet/Q-Sessison1-Basic_about_network.js
@@ -0,0 +1,33 @@
+# Phần 1.Các khái niệm cơ bản trong mạng máy tính
+=================================================
+1.Topology là gì?
+    - Là cấu trúc hình học không gian mà thực chất là cách bố trí phần tử của mạng cũng như cách nối giữa chúng với nhau.
+  1.1.Các loại cấu trúc mạng?
+  		-Mạng dạng hình sao(star topology ).
+  		-Mạng dạng vòng(ring topology).
+  		-Mạng dạng tuyến(linear bus topology).
+  		-Mạng dạng hỗn hợp.
+  1.2.Ưu điểm và nhược điểm của từng loại.
+  		---(Cần bổ sung)
+2.Protocol là gì?
+    - Là cách thức mà dữ liệu được truyển đi trên mạng.
+3.LAN/WAN/MAN/SAN là gì?
+    -
+4.Mô hình client/server là gì?
+    -
+5.Mô hình peer-to-peer là gì?
+    -Mạng ngang hàng trong đó hoạt động của mạng chủ yếu dựa vào khả năng tính toán và băng thông của các máy tham gia
+    chứ không phải tập trung vào một số nhỏ các máy chủ trung tâm như các mạng thông thường.
+6.Bandwidth là gì?
+    -Là lượng thông tin có thể truyền đi trên một kết nối mạng trong một khoảng thời gian.
+    -Lý tưởng.
+    -Đơn vị tính:bit/s(bps),Mbps,Gbps,...
+7.VPN là gì?
+    -Là mạng riêng ảo(virtual private network) là một mạng dành riêng để kết nối các máy tính của các công ty
+    tập đoàn hay các tổ chức với nhau thông qua mạng internet công cộng.
+8.Intranet là gì?
+   -Là mạng nội bộ.
+   -Có cấu trúc thượng tầng.
+9.Extranet là gì?
+   -Bao gồm intranet.
+   -Cho phép bên ngoài truy cập thông qua chứng thực.
