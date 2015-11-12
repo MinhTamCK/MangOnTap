416e8ea61f90120cd67d00336e8af9d63491ae89
tam.nm <tam.nm@sutrixmedia.com>
2015-11-12 17:31:34 +0700

    add file.

---
+++ LapTrinhWeb/Ly Thuyet/Q-Sessison4-IP.js
@@ -0,0 +1,10 @@
+# Phần 4.IP
+=================================================
+1.Địa chỉ IP là gì?
+2.Subnet là gì?
+	- Là chia một mạng lớn ra thành nhiều mạng con nhỏ hơn thì các mạng nhỏ này được gọi là subnet.
+3.Subnet mask là gì?
+	-Là một con số gồm có 32 bit gồm n bit 1 dùng để đánh địa chỉ mạng con và m bit 0 để đánh địa chỉ máy trong mạng con. n+m = 32.
+4.Địa chỉ Broadcast là gì?
+	-Là IP cuối cùng trong một mạng với tất cả các bit của host đều bật lên 1.
+	-Khi một gói tin gửi vào địa chỉ Broadcast thì gói tin này sẽ được phát tán đến tất cả các máy tính nằm trong mạng đó.
