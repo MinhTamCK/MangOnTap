81da0abbbe44f5ed32232b8913ace134452e18f1
tam.nm <tam.nm@sutrixmedia.com>
2015-11-30 17:51:12 +0700

    add file.

---
+++ NhapMonLapTrinh/Bai Tap/abc_project/app/scripts/plugins/print-button.js
@@ -0,0 +1,57 @@
+/**
+ *  @name plugin search call ajax
+ *  @description when user search then call ajax show recommned
+ *  @version 1.0
+ *  @options
+ *    option
+ *  @events
+ *    event
+ *  @methods
+ *    init
+ *    destroy
+ */
+;(function($, window, undefined) {
+  'use strict';
+
+  var pluginName = 'print-button';
+
+  function Plugin(element, options) {
+    this.element = $(element);
+    this.options = $.extend({}, $.fn[pluginName].defaults, this.element.data(), options);
+    this.init();
+  }
+
+  Plugin.prototype = {
+    init: function() {
+      var that = this,
+        el = that.element;
+      el.on('click', function(event) {
+        event.preventDefault();
+        window.print();
+      });
+    },
+    destroy: function() {
+      // remove events
+      // deinitialize
+      $.removeData(this.element[0], pluginName);
+    }
+  };
+
+  $.fn[pluginName] = function(options, params) {
+    return this.each(function() {
+      var instance = $.data(this, pluginName);
+      if (!instance) {
+        $.data(this, pluginName, new Plugin(this, options));
+      } else if (instance[options]) {
+        instance[options](params);
+      }
+    });
+  };
+
+  $.fn[pluginName].defaults = {};
+
+  $(function() {
+    $('[data-' + pluginName + ']')[pluginName]();
+  });
+
+}(jQuery, window));
